d99f8ccc798e1889cde82eabb3bd6fb3f243e715
lichen <lichen@itcast.cn>
2020-09-14 13:34:52 +0800

    修改

--- myGit/src/com/itheima/git/firstClass.java
+++ myGit/src/com/itheima/git/firstClass.java
@@ -3,9 +3,11 @@ package com.itheima.git;
 public class firstClass {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("helo,git!");
     }
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 }
